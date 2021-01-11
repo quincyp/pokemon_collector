f4408c534a8a6ac07931f6d5f88ee77f49b434b8
qtpham <quincytpham@gmail.com>
2021-01-11 10:04:51 -0800

    Added render for about route

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return HttpResponse('Test, working')
+    return HttpResponse('Pokemon Test, working')
+
+def about(request):
+    return render(request, 'about.html')
