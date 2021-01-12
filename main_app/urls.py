503dd51977d5f9ff004d9ba1874231487b1881d8
qtpham <quincytpham@gmail.com>
2021-01-11 16:01:31 -0800

    added pokemon folder/index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('pokemon/', views.pokemon_index, name='pokemon_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def pokemon_index(request):
+    return render(request, 'pokemon/index.html')
